cf601c8436e23ff157e00b14f589347bbf187b0a
Tatsuya <yokokura0413@gmail.com>
2021-02-07 00:55:00 +0900

    render from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "./firebase";
 
-function App() {
-  return <div></div>;
-}
+// interface TaskData {
+//   id: string;
+//   title: string;
+// }
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+
+  useEffect(() => {
+    const onSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            title: doc.data().title,
+          };
+        })
+      );
+    });
+    return () => onSub();
+  }, []);
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <p>{task.title}</p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
