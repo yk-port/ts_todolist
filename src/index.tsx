23752670a3a444015a23ef6710e62fad9f06ab33
Tatsuya <yokokura0413@gmail.com>
2021-02-07 08:29:11 +0900

    add login

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
